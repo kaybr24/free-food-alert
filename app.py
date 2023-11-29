0e855b21dd68e54171c2321296bcc9ce67c55933
Kayley <kw102@cs.wellesley.edu>
2023-11-29 10:17:49 -0500

    fixed information.py and some parts of searching

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import insert
 import profile
 import register
 import bcrypt
+import information
 from datetime import datetime, timedelta
 
 app.secret_key = 'your secret here'
@@ -62,24 +63,10 @@ def rate_post():
 @app.route('/search/', methods = ['GET', 'POST'])
 def search_posts():
     conn = dbi.connect()
-    locations=['Acorns', 'Alumnae Hall', 'Athletic Maintenance Facility', 'Bates Hall', 
-    'Beebe Hall', 'Billings', 'Boathouse', 'Campus Police Headquarters', 'Cazenove Hall', 
-    'Cedar Lodge', 'Cervantes', 'Cheever House', 'Child Study Center', 'Claflin Hall', 
-    'Collins Cinema', 'Continuing Education Office', 'Davis Hall', 'Davis Museum', 
-    'Davis Parking Facility', 'Day Care Center', 'Distribution Center', 'Dower House', 
-    'East Lodge', 'Fiske House', 'Founders Hall', 'Freeman Hall', 'French House - Carriage', 
-    'French House - Main', 'Golf House', 'Green Hall', 'Grounds', 'Hallowell House', 'Harambee House', 
-    'Hemlock', 'Homestead', 'Horton House', 'Instead', 'Jewett Art Center', 'Keohane Sports Center', 
-    'Lake House', 'Library', 'Lulu Chow Wang Campus Center', 'Margaret Ferguson Greenhouses', 
-    'McAfee Hall', 'Motor Pool', 'Munger Hall', 'Nehoiden House', 'Observatory', 'Orchard Apts', 
-    'Pendleton Hall East', 'Pendleton Hall West', 'Physical Plant', 'Pomeroy Hall', 
-    "President's House", 'Ridgeway Apts', 'Schneider Center', 'Science Center', 'Service Building', 
-    'Severance Hall', 'Shafer Hall', 'Shakespeare', 'Shepard House', 'Simpson Hall', 'Simpson West', 
-    'Slater International Center', 'Stone Center', 'Stone Hall', 'Tower Court East', 'Tower Court West',
-     'Trade Shops Building', 'Tau Zeta Epsilon', 'Waban House', 'Weaver House', 'Webber Cottage', 
-     'Wellesley College Club', 'West Lodge', 'Whitin House', 'Zeta Alpha House']
+    locations = information.locations
 
-    possible_allergens = ['Soy', 'Peanuts', 'Dairy', 'Eggs', 'Shellfish', 'Nuts', 'Sesame', 'Gluten']
+    possible_allergens = information.possible_allergens
+
     if request.method == 'POST':
         location = request.form.getlist('location')
         allergens = request.form.getlist('allergens')
--- information.py
+++ information.py
@@ -1,6 +1,7 @@
- locations=['Acorns', 'Alumnae Hall', 'Athletic Maintenance Facility', 'Bates Hall', 
-    'Beebe Hall', 'Billings', 'Boathouse', 'Campus Police Headquarters', 'Cazenove Hall', 
-    'Cedar Lodge', 'Cervantes', 'Cheever House', 'Child Study Center', 'Claflin Hall', 
+
+
+locations=['Acorns', 'Alumnae Hall', 'Athletic Maintenance Facility', 'Bates Hall', 'Beebe Hall', 'Billings', 'Boathouse', 
+    'Campus Police Headquarters', 'Cazenove Hall','Cedar Lodge', 'Cervantes', 'Cheever House', 'Child Study Center', 'Claflin Hall', 
     'Collins Cinema', 'Continuing Education Office', 'Davis Hall', 'Davis Museum', 
     'Davis Parking Facility', 'Day Care Center', 'Distribution Center', 'Dower House', 
     'East Lodge', 'Fiske House', 'Founders Hall', 'Freeman Hall', 'French House - Carriage', 
@@ -12,7 +13,6 @@
     "President's House", 'Ridgeway Apts', 'Schneider Center', 'Science Center', 'Service Building', 
     'Severance Hall', 'Shafer Hall', 'Shakespeare', 'Shepard House', 'Simpson Hall', 'Simpson West', 
     'Slater International Center', 'Stone Center', 'Stone Hall', 'Tower Court East', 'Tower Court West',
-     'Trade Shops Building', 'Tau Zeta Epsilon', 'Waban House', 'Weaver House', 'Webber Cottage', 
-     'Wellesley College Club', 'West Lodge', 'Whitin House', 'Zeta Alpha House']
-
+    'Trade Shops Building', 'Tau Zeta Epsilon', 'Waban House', 'Weaver House', 'Webber Cottage', 
+    'Wellesley College Club', 'West Lodge', 'Whitin House', 'Zeta Alpha House']
 possible_allergens = ['Soy', 'Peanuts', 'Dairy', 'Eggs', 'Shellfish', 'Nuts', 'Sesame', 'Gluten']
