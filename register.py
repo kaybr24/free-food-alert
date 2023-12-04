74b1c93eb36c72401f50e31f1e99bcdec4ace7db
Kayley <kw102@cs.wellesley.edu>
2023-12-04 10:43:59 -0500

    updated documentation and updated insert new post -- become a food guide if not

--- register.py
+++ register.py
@@ -14,8 +14,11 @@ def register_user(conn, full_name, wellesley_email, password, date):
     return True  # successfully registered
 
 def check_user_exists(conn, wellesley_email):
-    query = "SELECT * FROM users WHERE wellesley_email = %s"
-    result = dbi.query(conn, query, (wellesley_email,), result_set=True)
+    curs = dbi.dict_cursor(conn)
+    # query = "SELECT * FROM users WHERE wellesley_email = %s", [wellesley_email]
+    curs.execute('''SELECT * from user where user_email=%s ''', [wellesley_email])
+    result = curs.fetchall()
+    # result = dbi.query(conn, query, (wellesley_email,), result_set=True)
     
     # If the result set is not empty, the user exists
     return len(result) > 0
