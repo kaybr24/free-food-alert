a59fb1f14b1d6e0dc1228e81ad91177130af1b8d
Kayla Brand <kb102@cs.wellesley.edu>
2023-12-18 10:53:07 -0500

    url-for

--- search.py
+++ search.py
@@ -5,26 +5,10 @@ import information
 
 legalAllergens = set(['soy', 'peanuts', 'dairy', 'gluten', 'eggs', 'shellfish', 'nuts', 'sesame'])
 legalBuildings = set(information.locations)
-# legalBuildings = set(['Acorns', 'Alumnae Hall', 'Athletic Maintenance Facility', 'Bates Hall', 
-#     'Beebe Hall', 'Billings', 'Boathouse', 'Campus Police Headquarters', 'Cazenove Hall', 
-#     'Cedar Lodge', 'Cervantes', 'Cheever House', 'Child Study Center', 'Claflin Hall', 
-#     'Collins Cinema', 'Continuing Education Office', 'Davis Hall', 'Davis Museum', 
-#     'Davis Parking Facility', 'Day Care Center', 'Distribution Center', 'Dower House', 
-#     'East Lodge', 'Fiske House', 'Founders Hall', 'Freeman Hall', 'French House - Carriage', 
-#     'French House - Main', 'Golf House', 'Green Hall', 'Grounds', 'Hallowell House', 'Harambee House', 
-#     'Hemlock', 'Homestead', 'Horton House', 'Instead', 'Jewett Art Center', 'Keohane Sports Center', 
-#     'Lake House', 'Library', 'Lulu Chow Wang Campus Center', 'Margaret Ferguson Greenhouses', 
-#     'McAfee Hall', 'Motor Pool', 'Munger Hall', 'Nehoiden House', 'Observatory', 'Orchard Apts', 
-#     'Pendleton Hall East', 'Pendleton Hall West', 'Physical Plant', 'Pomeroy Hall', 
-#     "President's House", 'Ridgeway Apts', 'Schneider Center', 'Science Center', 'Service Building', 
-#     'Severance Hall', 'Shafer Hall', 'Shakespeare', 'Shepard House', 'Simpson Hall', 'Simpson West', 
-#     'Slater International Center', 'Stone Center', 'Stone Hall', 'Tower Court East', 'Tower Court West',
-#      'Trade Shops Building', 'Tau Zeta Epsilon', 'Waban House', 'Weaver House', 'Webber Cottage', 
-#      'Wellesley College Club', 'West Lodge', 'Whitin House', 'Zeta Alpha House'])
 
 def search_for_post(conn, searched_item):
     """
-    Given a dictionary if items searched for by the user, 
+    Given a dictionary of items searched for by the user, 
     finds if there are any posts with specified search
     and returns the post.
     """
@@ -66,7 +50,7 @@ def search_for_post(conn, searched_item):
         formatted_date = datetime.strptime(searched_item['date_posted'], '%Y-%m-%d').strftime('%Y-%m-%d')
 
         if 'building' in query or 'allergens' in query:
-            query += " AND post_date = '{}'".format(formatted_date)
+            query += " AND post_date >= '{}'".format(formatted_date)
         else:
             query += " post_date = '{}'".format(formatted_date)
       
